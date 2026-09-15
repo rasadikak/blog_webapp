1a7c92261d69d95c2fb6cdf9d3c4d733c2cc181b
kaushani rasadika <rasadikak@gmail.com>
2026-09-15 11:09:45 +0530

    fix:commets model  issue

--- backend/Models/Comment.cs
+++ backend/Models/Comment.cs
@@ -3,7 +3,9 @@ public class Comment
     public int Id { get; set; }
     public required string Content { get; set; }
     public DateTime CreatedAt { get; set; }
+
     public int PostId { get; set; }
-    public Post Post{get; set;}= null!;
+    public Post? Post { get; set; }
+
     public required string CommenterName { get; set; }
 }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login/>} />
         <Route path="/dashboard" element={<Dashboard/>} />
-        <Route path="/post" element={<Post/>} />
+        <Route path="/post/:id" element={<Post/>} />
 
       </Routes>
     </BrowserRouter>
